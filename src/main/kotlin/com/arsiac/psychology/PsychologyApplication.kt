d496a1973fd54a68992e116fe9baa979535ab0cd
arsiac <arsia.chou@gmail.com>
2021-01-22 09:04:21 +0800

    init

---
+++ src/main/kotlin/com/arsiac/psychology/PsychologyApplication.kt
@@ -0,0 +1,11 @@
+package com.arsiac.psychology
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PsychologyApplication
+
+fun main(args: Array<String>) {
+	runApplication<PsychologyApplication>(*args)
+}
