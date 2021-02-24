0477ac9af9c7fd534e4f0d2d623829c831f46416
arsiac <arsia.chou@gmail.com>
2021-02-24 10:25:35 +0800

    统一回复

---
+++ psychology-utils/src/main/java/top/arsiac/psychology/utils/aspect/CommonControllerAdvice.java
@@ -0,0 +1,47 @@
+package top.arsiac.psychology.utils.aspect;
+
+import com.github.pagehelper.Page;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.lang.NonNullApi;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+import top.arsiac.psychology.utils.common.Reply;
+
+/**
+ * <p>统一返回值</p>
+ *
+ * @author arsiac
+ * @version 1.0
+ * @since  2021/2/24
+ */
+@RestControllerAdvice
+public class CommonControllerAdvice implements ResponseBodyAdvice<Object> {
+    @Override
+    public boolean supports(MethodParameter methodParameter, Class<? extends HttpMessageConverter<?>> aClass) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object o, MethodParameter methodParameter, MediaType mediaType, Class<? extends HttpMessageConverter<?>> aClass, ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse) {
+        // 不拦截 Reply
+        if (o instanceof Reply) {
+            return o;
+        }
+
+        // 分页
+        if (o instanceof Page) {
+            Page<?> page = (Page<?>) o;
+            return Reply.success()
+                    .data(o)
+                    .total(page.getTotal())
+                    .pageSize(page.getPageSize())
+                    .pageNum(page.getPageNum());
+        }
+
+        return Reply.success().data(o);
+    }
+}
