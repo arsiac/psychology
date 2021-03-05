dde73a4f6c18f2e4150a7236fbdc4f8663d1c76f
arsiac <arsia.chou@gmail.com>
2021-03-05 11:56:55 +0800

    fix bug: 查询角色为空

--- centre/src/main/java/com/github/arsiac/psychology/centre/api/controller/UserRoleController.java
+++ centre/src/main/java/com/github/arsiac/psychology/centre/api/controller/UserRoleController.java
@@ -25,48 +25,49 @@ public class UserRoleController implements UserRoleApi {
      * */
     private UserRoleService userRoleService;
 
+    @SystemLogger("查询全部用户的角色")
     @Override
     public List<UserRoleVO> queryAll() {
         return BeanCopy.copyList(userRoleService.queryAll(), UserRoleVO.class);
     }
 
-    @SystemLogger
+    @SystemLogger("根据角色id查询")
     @Override
     public List<UserRoleVO> queryByRoleId(Long id) {
         return BeanCopy.copyList(userRoleService.queryByRoleId(id), UserRoleVO.class);
     }
 
-    @SystemLogger
+    @SystemLogger("根据用户id查询")
     @Override
     public List<UserRoleVO> queryByUserId(Long id) {
-        return BeanCopy.copyList(userRoleService.queryByRoleId(id), UserRoleVO.class);
+        return BeanCopy.copyList(userRoleService.queryByUserId(id), UserRoleVO.class);
     }
 
-    @SystemLogger
+    @SystemLogger("增加用户的角色")
     @Override
     public boolean add(UserRoleDTO dto) {
         return userRoleService.add(dto);
     }
 
-    @SystemLogger
+    @SystemLogger("批量增加用户的角色")
     @Override
     public boolean batchAdd(List<UserRoleDTO> dtoList) {
         return userRoleService.batchAdd(dtoList);
     }
 
-    @SystemLogger
+    @SystemLogger("修改用户的角色")
     @Override
     public boolean modify(UserRoleDTO dto) {
         return userRoleService.modify(dto);
     }
 
-    @SystemLogger
+    @SystemLogger("删除用户的角色")
     @Override
     public boolean remove(UserRoleDTO dto) {
         return userRoleService.remove(dto);
     }
 
-    @SystemLogger
+    @SystemLogger("批量删除用户的角色")
     @Override
     public boolean batchRemove(List<UserRoleDTO> dtoList) {
         return userRoleService.batchRemove(dtoList);
