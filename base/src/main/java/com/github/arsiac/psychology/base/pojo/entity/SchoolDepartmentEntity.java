7c4c2b328ff6bacdfd47ae5bc986c470b6ca0c1b
arsiac <arsia.chou@gmail.com>
2021-04-02 05:49:13 +0800

    \t改为空格

--- base/src/main/java/com/github/arsiac/psychology/base/pojo/entity/SchoolDepartmentEntity.java
+++ base/src/main/java/com/github/arsiac/psychology/base/pojo/entity/SchoolDepartmentEntity.java
@@ -12,108 +12,108 @@ import java.util.Date;
  */
 public class SchoolDepartmentEntity {
 
-	/**
-	 * 学院id
-	 */
-	private Long id;
+    /**
+     * 学院id
+     */
+    private Long id;
 
-	/**
-	 * 学院名称
-	 */
-	private String name;
+    /**
+     * 学院名称
+     */
+    private String name;
 
-	/**
-	 * 创建者
-	 */
-	private Long createBy;
+    /**
+     * 创建者
+     */
+    private Long createBy;
 
-	/**
-	 * 创建时间
-	 */
-	private Date createTime;
+    /**
+     * 创建时间
+     */
+    private Date createTime;
 
-	/**
-	 * 更新者
-	 */
-	private Long updateBy;
+    /**
+     * 更新者
+     */
+    private Long updateBy;
 
-	/**
-	 * 更新时间
-	 */
-	private Date updateTime;
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
 
-	/**
-	 * 版本
-	 */
-	private Integer version;
+    /**
+     * 版本
+     */
+    private Integer version;
 
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setCreateBy(Long createBy) {
-		this.createBy = createBy;
-	}
+    public void setCreateBy(Long createBy) {
+        this.createBy = createBy;
+    }
 
-	public Long getCreateBy() {
-		return createBy;
-	}
+    public Long getCreateBy() {
+        return createBy;
+    }
 
-	public void setCreateTime(Date createTime) {
-		this.createTime = createTime;
-	}
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
 
-	public Date getCreateTime() {
-		return createTime;
-	}
+    public Date getCreateTime() {
+        return createTime;
+    }
 
-	public void setUpdateBy(Long updateBy) {
-		this.updateBy = updateBy;
-	}
+    public void setUpdateBy(Long updateBy) {
+        this.updateBy = updateBy;
+    }
 
-	public Long getUpdateBy() {
-		return updateBy;
-	}
+    public Long getUpdateBy() {
+        return updateBy;
+    }
 
-	public void setUpdateTime(Date updateTime) {
-		this.updateTime = updateTime;
-	}
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
 
-	public Date getUpdateTime() {
-		return updateTime;
-	}
+    public Date getUpdateTime() {
+        return updateTime;
+    }
 
-	public void setVersion(Integer version) {
-		this.version = version;
-	}
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
 
-	public Integer getVersion() {
-		return version;
-	}
+    public Integer getVersion() {
+        return version;
+    }
 
-	@Override
-	public String toString() {
-		return "SchoolDepartmentEntity{" +
-				"id=" + id +
-				", name='" + name + '\'' +
-				", createBy=" + createBy +
-				", createTime=" + createTime +
-				", updateBy=" + updateBy +
-				", updateTime=" + updateTime +
-				", version=" + version +
-				'}';
-	}
+    @Override
+    public String toString() {
+        return "SchoolDepartmentEntity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", createBy=" + createBy +
+                ", createTime=" + createTime +
+                ", updateBy=" + updateBy +
+                ", updateTime=" + updateTime +
+                ", version=" + version +
+                '}';
+    }
 }
