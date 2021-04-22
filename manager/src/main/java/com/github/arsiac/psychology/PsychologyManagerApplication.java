bade57814b32951973c550fc0ebe7904bdf98b83
arsiac <arsia.chou@gmail.com>
2021-04-23 05:33:35 +0800

    本地数据库

--- manager/src/main/java/com/github/arsiac/psychology/PsychologyManagerApplication.java
+++ manager/src/main/java/com/github/arsiac/psychology/PsychologyManagerApplication.java
@@ -2,7 +2,10 @@ package com.github.arsiac.psychology;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
@@ -16,6 +19,15 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableEurekaClient
 @SpringBootApplication
 public class PsychologyManagerApplication {
+    /**
+     * 服务调用
+     * */
+    @Bean
+    @LoadBalanced
+    public RestTemplate createRestTemplate() {
+        return new RestTemplate();
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(PsychologyManagerApplication.class, args);
     }
